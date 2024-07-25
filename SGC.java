77ec47680de34d3c1484ce4d034165cd96dab383
Shailendra751851 <113360258+Shailendra751851@users.noreply.github.com>
2024-07-25 23:00:20 +0530

    Update SGC.java

--- SGC.java
+++ SGC.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SGC{
+public class StudentGradeCalculator{
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.println("*******************Student grade Calculator**************");
